ba7826dc6dba9be35695f5badef4fa882ea01ebc
Francisco Karriere <francisco.karriere@gmail.com>
2022-04-22 01:51:12 -0500

    agregado de console agrupado y constantes para cuadrado y triangulo

--- figuras.js
+++ figuras.js
@@ -1,1 +1,29 @@
 console.log("Hola mundo");
+
+// Ejercicio 1 codigo del cuadrado
+console.group("cuadrado");
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado son: " + ladoCuadrado + "cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es: " + perimetroCuadrado + "cm");
+
+const areaCuaadrado = ladoCuadrado * ladoCuadrado;
+console.log("El area del cuadrado es: " + areaCuaadrado + "cm cuadrados");
+console.groupEnd();
+
+// codigo del triangulo
+console.group("Triangulo");
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const ladoTriangulo3 = 4;
+console.log(
+  "Los lados del triangulo son: " +
+    ladoTriangulo1 +
+    "cm, " +
+    ladoTriangulo2 +
+    "cm y " +
+    ladoTriangulo3 +
+    "cm"
+);
+console.groupEnd();
